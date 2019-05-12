4389d05512af4b573af05657b7a227ac12b07fe1
brunos-stash <topkekmp4@gmail.com>
2019-05-12 21:06:11 +0200

    more features and twice as fast because of numpy

--- Drop.py
+++ Drop.py
@@ -21,8 +21,6 @@ class Drop:
         # width specifies the number of characters used in total to display, but if value needs more space than the width specifies then the additional space is used.
         # precision indicates the number of characters used after the decimal point
         print ( f'x:{self.x:7.2f} | y:{self.y:7.2f}' )
-
-
 class DropMaker:
     '''
     Contains array of drops with x, y position and speed value
@@ -68,25 +66,108 @@ class DropMaker:
     # def get_array(self):
     #     arr = np.array([self.x, self.x, self.speed])
 
+@dataclass
+class DropNP:
+    def __init__(self, x, y, speed):
+        # 1000mm = 1m
+        self.array = np.array([x,y,speed])
+        self.x = self.array[0]
+        self.y = self.array[1]
+        self.speed = self.array[2]
 
+        # array: np.array([x,y,speed], dtype=int)
+
+    def fall(self, time=1):
+        # # self.x = self.x + self.speed*time
+        # self.array[1] = self.array[1] + self.array[2]*time
+        self.y -= self.speed*time
+        # print(self.y)
+        # print(self.speed)
+        # print(self.array[1])
+
+    def getpos(self):
+        return (self.x, self.y)
+
+    def getspeed(self):
+        return self.speed
+
+    def showpos(self):
+        # f'{value:{width}.{precision}}'
+        # value is any expression that evaluates to a number
+        # width specifies the number of characters used in total to display, but if value needs more space than the width specifies then the additional space is used.
+        # precision indicates the number of characters used after the decimal point
+        print ( f'x:{self.x:7.2f} | y:{self.y:7.2f}' )
+
+class DropMakerNP:
+    '''
+    Contains array of drops with x, y position and speed value
+
+    `dropcounter`:
+        how man drops
+    `speed`:
+        y speed in meter/sec
+    `xbound=10000`:
+        boundary 0-xbound X axis,
+        1 unit = 1 mm
+        default is 10meter
+    `ybound=10000`:
+        boundary 0-ybound Y axis,
+        1 unit = 1 mm
+        default is 10meter
+    '''
+
+    def __init__(self, dropcounter, speed, xbound=10000, ybound=10000, dropsize=1):
+        self.xbound = xbound
+        self.ybound = ybound
+        self.dropsize = dropsize
+        self.droplist = np.empty(dropcounter, dtype=DropNP)
+        # speed * 1000 because smallest drop will be 1 mm
+        speed = speed * 1000 
+        for _ in range(dropcounter):
+            rx = random.randint(0, xbound) 
+            ry = random.randint(0, ybound)
+            # 3,8 m/s =>3800 mm/s 
+            drop = DropNP(rx,ry,speed)
+            self.droplist[_] = drop
+        # print(self.droplist)
+        
+    def fall(self, time=1):
+        for drop in self.droplist:
+            drop.fall(time=time)
+            if drop.y <= 0:
+                drop.y = self.ybound
+            
+    def showpos(self, dropid=0):
+        '''
+        Prints position of the drop in `self.droplist`
+        '''
+        print ( f'x:{self.droplist[dropid].x/1000:7.2f}m | y:{self.droplist[dropid].y/1000:7.2f}m | speed:{self.droplist[dropid].speed/1000:5.2f}m/s' )
+
+    def getpos(self, dropid=0):
+        '''
+        returns position of the drop in `self.droplist` as string
+        '''
+        return f'x:{self.droplist[dropid].x/1000:7.2f}m | y:{self.droplist[dropid].y/1000:7.2f}m | speed:{self.droplist[dropid].speed/1000:5.2f}m/s'
+
+    
 
 if __name__ == "__main__":
     from time import sleep
-    # time
-    updateintervall = 0.5
-    duration = 5
-    speed = 3.888888888888889 # 14 km/h-
-    # d = Drop(0,0,3.888888888888889)
-    d = DropMaker(1, speed)
 
+    updateintervall = 0.1
+    duration = 1
+    speed = 1 # 14 km/h
+    # speed = 3.888888888888889 # 14 km/h
+    dropcounter = 3
+    
+    print()
+
+    d = DropMakerNP(dropcounter, speed, xbound=1, ybound=1)
+    print('liter: ', d.dropvolume())
     for time in range(duration):
         print(f'time:{time:3d}')
         # 1sec
         for _ in range(int(1/updateintervall)):
             d.fall()
+            d.showpos()
             sleep(updateintervall)
-    
-
-
-    #     d.fall(t)
-    #     d.showpos()
--- Sim.py
+++ Sim.py
@@ -1,26 +1,47 @@
-from Drop import Drop
+from Drop import DropMaker, DropMakerNP
 from time import sleep
 import numpy as np
 
-def simulate(time, dropcount):
-    updateintervall = 0.1
-    t = time
-    droplist = []
-    for _ in range(dropcount):
-        drop = Drop(0,0,3.888888888888889)
-        droplist.append(drop)
-    droparray = np.array(droplist)
+
+def simulate(duration=1, dropcount=100, rainspeed=3.888888888888889, update=0.1, dropsize=1):
+    '''
+    Simulates raindrops falling down
+
+    `duration`:how long letting it rain in seconds
+
+    `dropcount`:how many raindrops to produce
+
+    `rainspeed`:falling speed in m/s
+
+    `update`:time in seconds between updates for all raindrop positions
+    '''
     
+    def dropvolume(dropcount, dropsize):
+        volume = dropsize / 2 * np.pi * 4/3
+        fullvolume = volume*dropcount
+        liter = fullvolume / 1000000
+        return liter
+    
+    update = update
+    time_resolution = int(1/update)
+    d = DropMakerNP(dropcount, rainspeed)
+    print('Duration: ', duration)
+    print('Raindrops: ', dropcount)
+    print('Falling Speed: ', rainspeed)
+    print('liter: ', dropvolume(dropcount,dropsize))
 
-    for time in range(t):
-        print(f'time: {time:2d}')
-        for _ in range(10):
-            for drop in droparray:
-                drop.fall(t)
-            droparray[0].showpos()
-            sleep(updateintervall)
-        
-
+    # 1sec
+    for time in range(duration):
+        final_time = time
+        for _ in range(time_resolution):
+            final_time += update
+            d.fall(time=update)
+            # d.showpos()
+            pos = d.getpos()
+            stat = f'{pos} | time: {final_time:3.2f}s'
+            print(stat)
+            # sleep(updateintervall)
+    
 if __name__ == "__main__":
     # t = 0.1
     # d = Drop(0,0,3.888888888888889)
@@ -28,4 +49,8 @@ if __name__ == "__main__":
     #     d.fall(t)
     #     d.showpos()
     #     sleep(t)
-    simulate(5,1000000)
+    # duration = 2
+    # dropcount = 100
+    # speed = 3.888888888888889
+    # timeit.timeit("simulate(duration, dropcount, speed)",setup="from __main__ import simulate",number=1)
+    simulate(dropcount=10000,update=1)
